6dbbf65f13c774dd50fb88ac6f3f342f0d67c32e
Legal-Dan <clemies2000@hotmail.com>
2023-10-27 13:54:38 +0100

    Pull trackerData into a separate file

---
+++ trackerData.js
@@ -0,0 +1,26 @@
+
+     function trackerData() {return [
+            // {
+            //     name: "Guards Guards!",
+            //     productPage: "https://www.amazon.co.uk/Guards-Discworld-City-Watch-Collection/dp/1473200180/",
+            //     identifier: ".a-color-price"
+            // },
+            // {
+            //     name: "Cyberpunk Red RPG Jumpstart Kit",
+            //     productPage: "https://magicmadhouse.co.uk/r-talsorian-games-cyberpunk-red-jumpstart-kit",
+            //     identifier: ".price--withTax"
+            // },
+            {
+                name: "KULT: The Forbidden",
+                productPage: "https://webshop.helmgast.se/kult/the-forbidden-scenario-collection.html",
+                identifier: ".tws-api--price-current"
+            },
+            // {
+            //     name: "Tales of Aquatic Terror",
+            //     productPage: "https://www.drivethrurpg.com/product/338827/Tales-of-Aquatic-Terror",
+            //     identifier: ".product-tile-text-highlight"
+            // }
+        ]
+     }
+
+     module.exports = trackerData();
