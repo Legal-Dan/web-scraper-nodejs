00662674234913b33a4904c9926179b7cbb5fbf3
Legal-Dan <clemies2000@hotmail.com>
2024-01-05 15:46:01 +0000

    Set up timed events to run price scraping at midnight

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const databaseHandler = require("./databaseHandler.js")
 const currencyStringToNumberInGbp = require("./currencyConversions.js");
 
 async function runApp() {
+    cron.schedule('0 0 * * *', () => {
+        startApp()
+    });
+}
+
+async function startApp() {
     for (let entry of trackerData) {
         const priceString = await scrapeData.getHTML(entry.productPage, entry.identifier);
         const currentPrice = await currencyStringToNumberInGbp(priceString);
-        console.log("--------------")
-        console.log(entry.name)
-        console.log(priceString)
-        console.log(currentPrice)
         databaseHandler.writeToDb(entry.id, currentPrice)
     }
 }
