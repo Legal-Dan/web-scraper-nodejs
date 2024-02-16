fa89809c17e66defc605ad3fd1b12506917d828b
Legal-Dan <clemies2000@hotmail.com>
2024-02-16 14:11:11 +0000

    Adding more tracker data

--- trackerData.js
+++ trackerData.js
@@ -29,6 +29,36 @@ function trackerData() {
             name: "In Strange Seas",
             productPage: "https://preview.drivethrurpg.com/en/product/455060/in-strange-seas-the-regency-cthulhu-naval-compendium",
             identifier: "Hardcover, Standard Color Book"
+        },
+        {
+            id: 6,
+            name: "No Security: Horror Scenarios in the Great Depression",
+            productPage: "https://www.drivethrurpg.com/product/122756/No-Security-Horror-Scenarios-in-the-Great-Depression",
+            identifier: "Softcover, Standard Color Book"
+        },
+        {
+            id: 7,
+            name: "Subversive Sci-Fi",
+            productPage: "https://www.drivethrurpg.com/product/428384/Subversive-SciFi",
+            identifier: "Hardcover, B&amp;W Book"
+        },
+        {
+            id: 8,
+            name: "The Haunting of Abbeyham Priory!",
+            productPage: "https://www.drivethrurpg.com/product/374132/The-Haunting-of-Abbeyham-Priory-A-Jumpstart-for-They-Came-From-Beyond-the-Grave",
+            identifier: "Softcover, Premium Color Book"
+        },
+        {
+            id: 9,
+            name: "Transgressive Horror: Reflections on Scare Films that Broke the Rules",
+            productPage: "https://www.drivethrurpg.com/product/378202/Transgressive-Horror-Reflections-on-Scare-Films-that-Broke-the-Rules",
+            identifier: "Hardcover, B&amp;W Book"
+        },
+        {
+            id: 10,
+            name: "Zgrozy: From Beyond scenario anthology",
+            productPage: "https://www.drivethrurpg.com/product/451445/Zgrozy-From-Beyond--scenario-anthology-for-Call-of-Cthulhu",
+            identifier: "Hardcover, Standard Color Book"
         }
     ]
 }
